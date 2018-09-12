1a15fecdcaea716fd8e6956adb6688810fd18ac1
Totta Wang <tottawang@hotmail.com>
2018-09-12 09:50:21 +0800

    add skeleton

---
+++ src/functionalInterface/PublisherSubscriber.java
@@ -0,0 +1,10 @@
+package functionalInterface;
+
+/**
+ * Following the idea and sample from 
+ * http://www.byteslounge.com/tutorials/java-8-consumer-and-supplier
+ *
+ */
+public class PublisherSubscriber {
+
+}
