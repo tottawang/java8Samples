aca2736f6a44fdaebba8fc0851c635b7d6c81f30
Totta Wang <tottawang@hotmail.com>
2018-09-22 21:07:12 +0800

    remove unnecessary comment

--- src/dynamicProxy/ProxySubject.java
+++ src/dynamicProxy/ProxySubject.java
@@ -14,7 +14,7 @@ public class ProxySubject implements InvocationHandler {
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		System.out.println("brfore call specific method >>" + method.getName());
-        Object result = method.invoke(target, args);//MethodAccessor.invoke()
+        Object result = method.invoke(target, args);
         System.out.println("after call specific method>>" + method.getName());
         return result;
 	}
